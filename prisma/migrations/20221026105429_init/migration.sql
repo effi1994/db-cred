c173cc9d0407ff17cbb9bca4981f3ae2111cdf5c
effi1994 <effi130894@gmail.com>
2022-10-26 14:54:54 +0300

    add api

---
+++ prisma/migrations/20221026105429_init/migration.sql
@@ -0,0 +1,61 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `published` on the `post` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the `profile` table. If the table is not empty, all the data it contains will be lost.
+  - A unique constraint covering the columns `[userName]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[phoneNumber]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `address` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `city` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `country` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `phoneNumber` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userName` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE `post` DROP FOREIGN KEY `Post_authorId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `profile` DROP FOREIGN KEY `Profile_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `post` DROP COLUMN `published`;
+
+-- AlterTable
+ALTER TABLE `user` DROP COLUMN `name`,
+    ADD COLUMN `address` VARCHAR(191) NOT NULL,
+    ADD COLUMN `city` VARCHAR(191) NOT NULL,
+    ADD COLUMN `country` VARCHAR(191) NOT NULL,
+    ADD COLUMN `firstName` VARCHAR(191) NOT NULL,
+    ADD COLUMN `lastName` VARCHAR(191) NOT NULL,
+    ADD COLUMN `password` VARCHAR(191) NOT NULL,
+    ADD COLUMN `phoneNumber` VARCHAR(191) NOT NULL,
+    ADD COLUMN `userName` VARCHAR(191) NOT NULL;
+
+-- DropTable
+DROP TABLE `profile`;
+
+-- CreateTable
+CREATE TABLE `_PostToUser` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_PostToUser_AB_unique`(`A`, `B`),
+    INDEX `_PostToUser_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_userName_key` ON `User`(`userName`);
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_phoneNumber_key` ON `User`(`phoneNumber`);
+
+-- AddForeignKey
+ALTER TABLE `_PostToUser` ADD CONSTRAINT `_PostToUser_A_fkey` FOREIGN KEY (`A`) REFERENCES `Post`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_PostToUser` ADD CONSTRAINT `_PostToUser_B_fkey` FOREIGN KEY (`B`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
